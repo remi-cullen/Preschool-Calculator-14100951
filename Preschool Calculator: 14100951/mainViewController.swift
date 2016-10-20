45e1ca5e933470243214eda362743bd41ba60dbe
Remi.UHCS <remstarcullen@yahoo.com>
2016-10-20 23:26:16 +0100

    Addition of images , audio and another view controller and view

---
+++ Preschool Calculator: 14100951/mainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  mainViewController.swift
+//  Preschool Calculator: 14100951
+//
+//  Created by Remi Cullen on 20/10/2016.
+//  Copyright © 2016 Remi Cullen. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+
+class mainViewController : ViewController{
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        //addition of 
+    }
+}
