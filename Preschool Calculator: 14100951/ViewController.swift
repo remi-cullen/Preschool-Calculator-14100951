673e33bfb22c174da35b42b668fe43856aea27d7
Remi.UHCS <remstarcullen@yahoo.com>
2016-10-20 13:10:33 +0100

    Initial Commit

---
+++ Preschool Calculator: 14100951/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Preschool Calculator: 14100951
+//
+//  Created by Remi Cullen on 20/10/2016.
+//  Copyright © 2016 Remi Cullen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
