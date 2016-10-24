d92963cf43139cfc315731ce4943b51c15071090
Remi.UHCS <remstarcullen@yahoo.com>
2016-10-24 12:08:38 +0100

    addition of math logic , along with aesthetic buttons

--- Preschool Calculator: 14100951/ViewController.swift
+++ Preschool Calculator: 14100951/ViewController.swift
@@ -11,14 +11,16 @@ import UIKit
 class ViewController: UIViewController
 {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidLoad()
+    {
+         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "calculateHomepageImage.png")!);
         
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
--- Preschool Calculator: 14100951/mainViewController.swift
+++ Preschool Calculator: 14100951/mainViewController.swift
@@ -10,17 +10,48 @@ import UIKit
 import Foundation
 
 
-class mainViewController : ViewController{
+class mainViewController : UIViewController
+{
+    //Variables
+    var  buttonInput = Float();//the input value from the number buttons
+    var firstNumberResult = Float();//firstNumResult
+    var secondNumberResult = Float();//secondNumResult
     
     //labels for displaying text
-    @IBOutlet weak var firstNumber: UILabel!
-    @IBOutlet weak var secondNumber: UILabel!
-    @IBOutlet weak var answerFromButton: UILabel!
-    @IBOutlet weak var answerFromCode: UILabel!
+    @IBOutlet weak var firstNumber: UILabel! //firstNum
+    @IBOutlet weak var secondNumber: UILabel! //2ndNum
+    @IBOutlet weak var answerFromButton: UILabel!//Answer
+    @IBOutlet weak var answerFromCode: UILabel! //Coded Answer
+    
+    func randomiseNumber()
+    {
+        firstNumber.text = "\(arc4random_uniform(4) + 1)";
+        secondNumber.text = "\(arc4random_uniform (4) + 1)";
+    }
+    
+    func calculateRandomNumbers()
+    {
+        //used to input the label value to a variable to be calculated
+        let firstNumberResult:Int? = Int(firstNumber.text!);
+        let secondNumberResult:Int? = Int(secondNumber.text!);
+        
+        let codedResult: Int = firstNumberResult! + secondNumberResult!; //final result
+        
+        answerFromCode.text = "\(codedResult)";
+    }
+    
+    @IBAction func inputNumButton(sender: UIButton)
+    {
+     buttonInput = Float(sender.titleLabel!.text!)!
+     answerFromButton.text = ("\(Int(buttonInput))");
+    }
     
     
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         //addition of
+        randomiseNumber();
+        calculateRandomNumbers();
     }
 }
